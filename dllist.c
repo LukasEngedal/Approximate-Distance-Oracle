84aae097e61a9a2c2ecdb021d35be408c586b1b3
Lukas Engedal <pqd294@alumni.ku.dk>
2016-02-12 17:14:22 +0100

    Forbedringer af dll

--- dllist.c
+++ dllist.c
@@ -3,20 +3,21 @@
 #include "dllist.h"
 
 void dllist_init(dllist_t *dllist) {
-    dllist->firstNode = NULL;
-    dllist->lastNode = NULL;
+  dllist->firstNode = NULL;
+  dllist->lastNode = NULL;
 }
 
-void dlnode_init(dlnode_t *dlnode) {
-    dlnode->prev = NULL;
-    dlnode->next = NULL;
-    dlnode->value = 0;
+void dlnode_init(dlnode_t *node) {
+  dlnode->prev = NULL;
+  dlnode->next = NULL;
+  dlnode->value = 0;
 }
 
-int dlnode_link(dlnode_t *dlnode1, dlnode_t *dlnode2) {
-    dlnode1->next = dlnode2;
-    dlnode2->prev = dlnode1;
-    return 0;
+int dlnode_insertAfter(dlnode_t *node, dlnode_t *newNode) {
+  newNode->next = node->next;
+  node->next = newNode;
+  newNode->prev = node;
+  return 0;
 }
 
 
--- dllist.h
+++ dllist.h
@@ -2,20 +2,23 @@
 #define DLLIST_H
 
 typedef struct dlnode_t {
-    struct dlnode_t* prev;
-    struct dlnode_t* next;
-    int value;
+  struct dlnode_t* prev;
+  struct dlnode_t* next;
+  int value;
 } dlnode_t;
 
 typedef struct dllist_t {
-    struct dlnode_t* firstNode;
-    struct dlnode_t* lastNode;
+  struct dlnode_t* firstNode;
+  struct dlnode_t* lastNode;
 } dllist_t;
 
 /* Initializes a doubly linked list */
 void dllist_init(dllist_t *dllist);
 
 /* Initializes a doubly linked list node */
-void dlnode_init(dlnode_t *dlnode);
+void dlnode_init(dlnode_t *node);
+
+/* Adds a node to a list */
+int dlnode_insertAfter(dlnode_t *node, dlnode_t *newNode);
 
 #endif
